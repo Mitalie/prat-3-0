Mitalie/prat-3-0

commit 895c37b880911721d7fb5d576ff23fe3c9388e0a

Started basic work on luacheck

addon/modules.lua
@@ -56,11 +56,11 @@ Module system flow:
 5) The module code creates a new module calling NewModule
 6) Once the module has been created, we recieve OnModuleCreated, we want to
    remember that this module is installed, so we save that info
-7) The module will have its on initalize called after Prat's.  
+7) The module will have its on initalize called after Prat's.
 8) If the module is disabled it will stop at this point
 9) If the module is enabled, it will call its OnEnable
 
-That give us the states: EXISTS, INSTALLED, INITIALIZED, ENABLED, 
+That give us the states: EXISTS, INSTALLED, INITIALIZED, ENABLED,
                                                          DISABLED
 
 
@@ -302,7 +302,7 @@ do
 end
 
 
---[[ 
+--[[
 
 For module options, i want to use the single closure style executed from the main chunk of the module,
 such as:
@@ -311,10 +311,10 @@ SetModuleOptionTable(name, function() return { ... } )
 
 This way the options can be GC'd by from the modules, before the decision is made as
 to whether we will actually load the module. they will go into a table here, and either
-free'd, given back to the module for it to execute, or possible executed on this end. 
+free'd, given back to the module for it to execute, or possible executed on this end.
 
 In any case there will only be 1 copy of the closure, and if executed it will create its data
-and then it can be freed leaving no code behind. Prat 2.0 used alot of memory solely because of 
+and then it can be freed leaving no code behind. Prat 2.0 used alot of memory solely because of
 its options tables, this tries to avoid that.
 
 ]]
@@ -355,7 +355,7 @@ do
   local function loadNow(self, mod)
     local success, ret = pcall(mod)
     if not success then
-      _G.geterrorhandler()(ret)
+      geterrorhandler()(ret)
     end
   end
 
@@ -363,7 +363,7 @@ do
     for i = 1, #modules_toload, 1 do
       local success, ret = pcall(modules_toload[i])
       if not success then
-        _G.geterrorhandler()(ret)
+        geterrorhandler()(ret)
       end
       modules_toload[i] = nil
     end
@@ -372,7 +372,7 @@ do
     for i = 1, #extensions_toload, 1 do
       local success, ret = pcall(extensions_toload[i])
       if not success then
-        _G.geterrorhandler()(ret)
+        geterrorhandler()(ret)
       end
       extensions_toload[i] = nil
     end
@@ -383,4 +383,4 @@ do
     AddModuleExtension = loadNow
   end
 end
-	
+

services/textcolor.lua
@@ -28,7 +28,6 @@
 --[[ BEGIN STANDARD HEADER ]] --
 
 -- Imports
-local _G = _G
 local tostring = tostring
 local select = select
 local type = type
@@ -42,7 +41,7 @@ setfenv(1, select(2, ...))
 --[[ END STANDARD HEADER ]] --
 
 
-CLR = {}
+local CLR = {}
 
 CLR.DEFAULT = "ffffff" -- default to white
 CLR.LINK = {

.luacheckrc
@@ -7,7 +7,108 @@ exclude_files = {
 ignore = {
 	"11./SLASH_.*", -- Setting an undefined (Slash handler) global variable
 	"11./BINDING_.*", -- Setting an undefined (Keybinding header) global variable
+	"21.", -- Unused argument
 }
 globals = {
+  -- Saved Variables
+  "Prat3CharDB",
+  "Prat3HighCPUPerCharDB",
+
+  -- Prat / Libraries
   "Prat",
+  "Prat_PopupFrame",
+  "Prat_PopupFrameText",
+  "LibStub",
+
+  -- Lua
+  "date",
+  "table.wipe",
+  "time",
+
+  -- Utility functions
+  "geterrorhandler",
+  "strsplit",
+  "tinsert",
+  "tremove",
+
+  -- WoW
+  "ACCEPT",
+  "CANCEL",
+  "CHAT_MSG_BN_WHISPER_INFORM",
+  "CHAT_MSG_CHANNEL_LIST",
+  "CHAT_MSG_INSTANCE_CHAT",
+  "CHAT_MSG_INSTANCE_CHAT_LEADER",
+  "CHAT_MSG_SAY",
+  "CHAT_MSG_WHISPER_INFORM",
+  "DEFAULT_CHAT_FRAME",
+  "LE_PARTY_CATEGORY_INSTANCE",
+  "LE_REALM_RELATION_SAME",
+  "NUM_CHAT_WINDOWS",
+  "SELECTED_CHAT_FRAME",
+
+  "ChatEdit_ChooseBoxForSend",
+  "ChatEdit_GetActiveWindow",
+  "ChatFrame1",
+  "ChatFrame1EditBox",
+  "ChatFrameChannelButton",
+  "ChatFrameMenuButton",
+  "ChatFrameToggleVoiceDeafenButton",
+  "ChatFrameToggleMuteButton",
+  "ChatFrame_OpenChat",
+  "GameFontNormal",
+  "UIParent",
+  "SlashCmdList",
+  "StaticPopupDialogs",
+  "StaticPopup_Show",
+
+  "Ambiguate",
+	"BNGetFriendInfo", -- Classic
+	"BNGetFriendInfoByID", -- Classic
+	"BNGetGameAccountInfo", -- Classic
+	"BNGetNumFriends",
+  "ChatTypeInfo",
+  "CreateFrame",
+  "GameTooltip",
+  "GetAverageItemLevel",
+  "GetBattlefieldScore",
+  "GetCVar",
+  "GetLocale",
+  "GetGuildRosterInfo",
+  "GetNumBattlefieldScores",
+  "GetNmGroupMembers",
+  "GetNumGuildMembers",
+  "GetMinimapZoneText",
+  "GetPlayerInfoByGUID",
+  "GetRaidTargetIndex",
+  "GetRealmName",
+  "GetRealZoneText",
+  "GetServerTime",
+  "GetTime",
+  "GetUnitName",
+  "IsCombatLog",
+  "IsControlKeyDown",
+  "IsInGroup",
+  "IsInGuild",
+  "IsInInstance",
+  "IsInRaid",
+  "IsShiftKeyDown",
+  "LoggingChat",
+  "LoggingCombat",
+  "PlaySound",
+  "UnitClass",
+  "UnitCreatureFamily",
+  "UnitCreatureType",
+  "UnitGUID",
+  "UnitExists",
+  "UnitHealth",
+  "UnitHealthMax",
+  "UnitIsFriend",
+  "UnitIsPlayer",
+  "UnitLevel",
+  "UnitName",
+  "UnitPower",
+  "UnitPowerMax",
+  "UnitRace",
+  "UnitSex",
+  "UnitRealmRelationship",
 }

commit a72ed734d5a6b8dadf0365243e7741a949b88367

Fix errors introduced with 895c37b

addon/modules.lua
@@ -38,6 +38,7 @@ local Prat = Prat
 local pcall = pcall
 local setmetatable = setmetatable
 local tinsert = tinsert
+local geterrorhandler = geterrorhandler
 -- Isolate the environment
 setfenv(1, select(2, ...))
 

services/textcolor.lua
@@ -42,6 +42,7 @@ setfenv(1, select(2, ...))
 
 
 local CLR = {}
+select(2, ...).CLR = CLR
 
 CLR.DEFAULT = "ffffff" -- default to white
 CLR.LINK = {